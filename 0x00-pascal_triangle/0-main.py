c900c7bd0f34b00dd6a1f880d9c334784fcfc3da
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-06-27 17:31:42 -0400

    Pascal's Triangle

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -2,7 +2,20 @@
 """
 0-main
 """
-from pascal_triangle import pascal_triangle
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
 
 def print_triangle(triangle):
     """
@@ -14,3 +27,4 @@ def print_triangle(triangle):
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+
