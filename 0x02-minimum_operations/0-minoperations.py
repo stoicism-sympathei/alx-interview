2e1e8e814e61b491f92c5ab255c58af0f9e9962f
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-07-11 10:36:25 -0400

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-'''
-Given a number n, write a method that calculates
-the fewest number of operations needed to result in
-exactly n H characters in the file.
-'''
+"""Minimum Operations module.
+
+This module contains a function that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
+
+"""
 
 
 def minOperations(n):
-    '''
-    returns min operations to get n Hs
-    '''
+    """Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+
+    Args:
+        n (int): the number of characters to reach.
+
+    Returns:
+        int: the fewest number of operations needed, 0 otherwise.
+
+    """
+    chars = n
     operations = 0
-    if n <= 1:
-        return 0
-    for i in range(2, n + 1):
-        '''
-        check if n could be broken into smaller parts
-        '''
-        while n % i == 0:
-            ''' reduce n into a smaller part '''
-            n = n / i
-            '''
-            if so add the nbr of smaller parts (in fact 1 copy and 4 paste)
-            '''
-            operations += i
+    div = 2
+
+    while chars > 1:
+        if chars % div == 0:
+            operations += div
+            chars /= div
+        else:
+            div += 1
+
     return operations
